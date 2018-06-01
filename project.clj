37f43da46790091d7743ab76486ba562fe8ceee1
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2018-06-01 08:07:07 -0400

    Use lein-bin

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.csv "0.1.4"]
                  [org.clojure/tools.cli "0.3.7"]]
-  :main ^:skip-aot ledger-reconciler.core
   :uberjar-name "ledger-reconciler.jar"
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :target-path "target"
+  :profiles {:uberjar {:aot :all}}
+  :main ledger-reconciler.core
+  :plugins [[lein-bin "0.3.4"]]
+  :bin {:name "ledger-reconciler"})
